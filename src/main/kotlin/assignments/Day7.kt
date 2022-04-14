02c71b016133f81c179b013033faaeca1ea8482a
Borek Beker <borek.beker@nos.nl>
2022-04-14 15:20:17 +0200

    Use single regex

--- src/main/kotlin/assignments/Day7.kt
+++ src/main/kotlin/assignments/Day7.kt
@@ -2,75 +2,56 @@ package assignments
 
 object Day7 {
 
-    fun runFirst(input: List<String> = input()): Int {
-        val parsedBags = createBagList(input)
-        return createBagList(input).filter { it.containsShinyGoldBag(parsedBags) }.size
+    fun runFirst(input: List<String> = input()): Int = createBagList(input).let { parsedBags ->
+        parsedBags.filter { containsShinyGoldBag(it, parsedBags) }.size
     }
 
-    fun runSecond(input: List<String> = input()): Int {
-        val parsedBags = createBagList(input)
-        return parsedBags.find { it.color == "shiny gold" }!!.sumAllBags(parsedBags)
+    fun runSecond(input: List<String> = input()): Int = createBagList(input).let { parsedBags ->
+        sumAllBags(parsedBags.find { it.color == "shiny gold" }!!, parsedBags)
     }
 
     // Parsing
 
-    private fun createBagList(input: List<String>): List<Bag> {
-        val parsedBags = mutableListOf<Bag>()
-        for (line in input) {
-            val createdBag = createBag(line, parsedBags)
-
-            // If did not already contain the bag, add a new one, or else put it at the index
-            parsedBags
-                .indexOfFirst { it.color == createdBag.color }
-                .takeIf { it >= 0 }
-                ?.let { parsedBags[it] = createdBag }
-                ?: parsedBags.add(createdBag)
-        }
-        return parsedBags
-    }
-
-    private fun createBag(line: String, parsedBags: MutableList<Bag>): Bag {
-        val bagColor = getBag(line)
-        val containingBags = getContainingBags(line).let { rawContainingBags ->
-            Array(rawContainingBags.size) { i ->
-                val (amount, color) = rawContainingBags[i]
-                amount to findOrCreateBag(parsedBags, color)
+    private fun createBagList(input: List<String>): List<Bag> = input.fold(mutableListOf()) { acc, line ->
+        createBag(line, acc).let { createdBag ->
+            acc.apply {
+                // If did not already contain the bag, add a new one, or else put it at the index
+                indexOfFirst { it.color == createdBag.color }.takeIf { it >= 0 }?.let { set(it, createdBag) } ?: add(createdBag)
             }
         }
-
-        return Bag(bagColor, containingBags)
     }
 
-    private fun getBag(line: String): String =
-        Regex("^[a-zA-Z]+ [a-zA-Z]+").find(line)!!.value
-
-    private fun getContainingBags(line: String): List<Pair<Int, String>> =
-        Regex("(?<amount>[0-9]) (?<color>[a-zA-Z]+ [a-zA-Z]+)")
+    private fun createBag(line: String, parsedBags: MutableList<Bag>): Bag =
+        Regex("(?<bagname>^[a-zA-Z]+ [a-zA-Z]+)|(?<amount>[0-9]) (?<containingbag>[a-zA-Z]+ [a-zA-Z]+)")
             .findAll(line)
             .toList()
-            .map { it.groups["amount"]?.value!!.toInt() to it.groups["color"]?.value!! }
+            .let { matches ->
+                val bagColor = matches.first { it.groups["bagname"] != null }.value
+                val containingBags = matches
+                    .mapNotNull { it.groups["amount"]?.value?.toInt() }
+                    .zip(matches.mapNotNull { it.groups["containingbag"]?.value })
+                    .let { rawContainingBags ->
+                        Array(rawContainingBags.size) { i ->
+                            val (amount, color) = rawContainingBags[i]
+                            amount to findOrCreateBag(parsedBags, color)
+                        }
+                    }
+
+                Bag(bagColor, containingBags)
+            }
 
     // Exercise calculation
 
-    private fun Bag.containsShinyGoldBag(allBags: List<Bag>): Boolean {
-        if (containsBags.isEmpty()) return false
+    private fun containsShinyGoldBag(bag: Bag, allBags: List<Bag>): Boolean =
+        bag.containsBags.any { it.second.color == "shiny gold" } ||
+            bag.containsBags.any { b -> containsShinyGoldBag(allBags.find { it.color == b.second.color }!!, allBags) }
 
-        return containsBags
-            .any { it.second.color == "shiny gold" } ||
-            containsBags.any { b -> allBags.find { it.color == b.second.color }!!.containsShinyGoldBag(allBags) }
-    }
+    private fun sumAllBags(bag: Bag, allBags: List<Bag>): Int =
+        bag.containsBags.sumOf { b -> b.first * sumAllBags(allBags.find { it.color == b.second.color }!!, allBags) }
+            .plus(bag.containsBags.sumOf { it.first })
 
-    private fun Bag.sumAllBags(allBags: List<Bag>): Int {
-        if (containsBags.isEmpty()) return 0
-
-        return containsBags
-            .sumOf { b -> b.first * allBags.find { it.color == b.second.color }!!.sumAllBags(allBags) }
-            .plus(containsBags.sumOf { it.first })
-    }
-
-    private fun findOrCreateBag(parsedBags: MutableList<Bag>, color: String): Bag {
-        return (parsedBags.find { it.color == color } ?: Bag(color, emptyArray()).also { parsedBags.add(it) })
-    }
+    private fun findOrCreateBag(parsedBags: MutableList<Bag>, color: String): Bag =
+        (parsedBags.find { it.color == color } ?: Bag(color, emptyArray()).also { parsedBags.add(it) })
 
     class Bag(val color: String, val containsBags: Array<Pair<Int, Bag>>)
 
